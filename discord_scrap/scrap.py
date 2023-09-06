59cf05d44aa5c0de3a4743e4e0661d2c1c6f756e
pedroaugtb <pedroaugustoatb@gmail.com>
2023-09-06 15:35:11 -0300

    llll

--- discord_scrap/scrap.py
+++ discord_scrap/scrap.py
@@ -1,21 +1,43 @@
 import requests
 import json
 
-def retrieve_messages(channel_id):
-
+#lalal
+def retrieve_messages(channel_id, output_file):
     headers = {
-        'authorization': 'NDQ0NjY1MDE0NTg3MDk3MDkw.GtDcGu.FteoelXcm8KpF7p3UkDnD4JcnYGu21NMsE7rzI'
+        'authorization': 'NjE5Njg1OTI1MzI4Mzg4MTE3.GA1otH.KMCFFmhcY6yRRoLbsXBcvKU_wJ4hqLQTXqa__s'
     }
 
-    message_request = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages?limit=50', headers=headers)
+    limit = 100
+    before = None
 
-    # messages_json = json.loads(message_request.text)
+    with open(output_file, 'w', encoding='utf-8') as file:
+        while True:
+            params = {
+                'limit': limit,
+                'before': before
+            }
 
-    with open ('data.json', 'w') as data_json:
-        json.dump(message_request.text, data_json)
+            message_request = requests.get(f'https://discord.com/api/v9/channels/150074202727251969/messages?limit=100', headers=headers, params=params)
 
-    
-    # for value in messages_json:
-    #     print(value, '\n')
+            if message_request.status_code == 200:
+                messages_json = json.loads(message_request.text)
 
-retrieve_messages(1148795977730379868)
+                if not messages_json:
+                    break
+
+                for message in messages_json:
+                    username = message['author']['username']
+                    content = message['content']
+                    alo = message['timestamp']
+                    id = message['id']
+                    message_str = f'{username}, {content}, {alo}, {id}\n'
+                    if(content != ''):
+                        print(message_str)
+                        file.write(message_str)
+
+                before = messages_json[-1]['id']  # ID da última mensagem na resposta
+            else:
+                print(f"Erro ao recuperar mensagens. Código de status: {message_request.status_code}")
+                break
+
+retrieve_messages('150074202727251969', 'mensagens.txt')
